7b1ae94d76cbdbdfa43c5608ba36e11a64c6dba0
belunatic <abelluvyou@yahoo.com>
2025-04-16 12:22:06 -0700

    moved the app.js from src folder to root folder

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
