a2f0200cd708d36a6b9af8c1519f6bf80e3e2703
belunatic <abelluvyou@yahoo.com>
2025-04-16 12:26:05 -0700

    added the axios package

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
-	res.send("Hello World!");
+	res.send(`this is the api ${process.env.NASA_API_KEY}`);
 });
 
 app.listen(PORT, () => {
